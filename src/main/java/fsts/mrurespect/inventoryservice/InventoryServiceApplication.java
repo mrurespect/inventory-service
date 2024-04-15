0b7a1cc290f3b6682305954c9d3015c21fdaf2c9
EL FADILI MOHAMED <580.simo@gmail.com>
2024-04-15 18:26:03 +0100

    inventory service :first commit

---
+++ src/main/java/fsts/mrurespect/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package fsts.mrurespect.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+}
