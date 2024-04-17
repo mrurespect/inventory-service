93dee565de914620b0a7943fee8513eb1b417c2d
EL FADILI MOHAMED <580.simo@gmail.com>
2024-04-17 19:45:03 +0100

    updation inventory service

--- src/main/java/fsts/mrurespect/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/fsts/mrurespect/inventoryservice/InventoryServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -13,8 +15,11 @@ public class InventoryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
-   @Bean
-    CommandLineRunner start(ProductRepository productRepository){
+
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository,
+                            RepositoryRestConfiguration restConfiguration){
+        restConfiguration.exposeIdsFor(Product.class);
     return args -> {
         for (int i = 1; i <= 20; i++) {
             productRepository.save(new Product(null,"Product " + i, i * 10));
